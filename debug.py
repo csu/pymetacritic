fd9377e0a2ed86dec9175734afce804356c73c1f
Christopher Su <christophersu9@gmail.com>
2015-01-17 11:45:18 -0500

    bugfix

--- debug.py
+++ debug.py
@@ -5,9 +5,13 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-result = metacritic.get_movie_critic('aa-dowd')
-with open('data_one_critic.json', 'w') as outfile:
-    json.dump(result, outfile)
+# result = metacritic.get_movie_critic('aa-dowd')
+# with open('data_one_critic.json', 'w') as outfile:
+#     json.dump(result, outfile)
 
-# metacritic.get_movie_critic('aa-dowd')
-metacritic.get_movie_critic('aaron-hillis')
+# test = metacritic.get_movie_critic('aa-dowd')
+# print test['highest_review_score']
+# print test['lowest_review_score']
+# metacritic.get_movie_critic('aaron-hillis')
+# metacritic.get_movie_critic('aaron-cutler')
+metacritic.get_movie_critic('ao-scott') 
