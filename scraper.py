ffa0a42506c7349213131c9aeaf4f68f824e7b2b
Christopher Su <christophersu9@gmail.com>
2015-01-17 11:09:52 -0500

    scraper improvements

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import metacritic
+import os
 import json
 from string import lowercase
 
@@ -6,19 +7,27 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-LETTER_RANGE = lowercase[0:6]
+LETTER_RANGE = lowercase[0:13]
 
 def save(obj, file_name):
     with open(file_name, 'w') as outfile:
         json.dump(obj, outfile)
 
-all_critics = []
+if not os.path.exists('data'):
+    os.makedirs('data')
+# if not os.path.exists('data/index'):
+#     os.makedirs('data/index')
+# if not os.path.exists('data/critics'):
+#     os.makedirs('data/critics')
+
+all_critics_index = []
 for letter in LETTER_RANGE:
     critics = metacritic.get_movie_critics_for_letter(letter)
-    save(critics, 'data/' + letter + '.json')
-    all_critics += critics
+    all_critics_index += critics
+    save(all_critics_index, 'data/index_' + str(LETTER_RANGE) + '.json')
 
+all_critics = []
 for critic in all_critics:
     slug = critic['critic_url'].replace('/critic/', '')
-    logging.debug('Scraping critic: ' + slug)
-    save(metacritic.get_movie_critic(slug), 'data/critics/' + slug + '.json')
+    all_critics.append(metacritic.get_movie_critic(slug))
+    save(all_critics, 'data/critics/' + str(LETTER_RANGE) + '.json')
