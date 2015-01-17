5b3eb7d3a52f28562a60786d88d3e087e7acf517
Christopher Su <christophersu9@gmail.com>
2015-01-17 00:56:47 -0500

    progress

--- debug.py
+++ debug.py
@@ -1,6 +1,9 @@
 import metacritic
 import json
 
-result = metacritic.get_all_movie_critics()
-with open('data.json', 'w') as outfile:
-    json.dump(result, outfile)
+# result = metacritic.get_all_movie_critics()
+# with open('data.json', 'w') as outfile:
+#     json.dump(result, outfile)
+
+metacritic.get_movie_critic('aa-dowd')
+metacritic.get_movie_critic('aaron-hillis')
--- metacritic/metacritic.py
+++ metacritic/metacritic.py
@@ -2,22 +2,22 @@ import urllib2
 import time
 from string import lowercase as lowercase_letters
 from bs4 import BeautifulSoup
+import re
 
-def keep_trying_to_get_html(letter):
+def keep_trying_to_get_html(url):
     try:
-        url = 'http://www.metacritic.com/browse/movies/critic/name/' + letter + '?num_items=100'
         request = urllib2.Request(url)
         request.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36')
         opener = urllib2.build_opener()
         html_doc = opener.open(request).read()
         return html_doc
     except:
-        # print 'error on: ' + letter + '. trying again.'
         time.sleep(3)
-        return keep_trying_to_get_html(letter)
+        return keep_trying_to_get_html(url)
 
 def get_movie_critics_for_letter(letter):
-    html_doc = keep_trying_to_get_html(letter)
+    url = 'http://www.metacritic.com/browse/movies/critic/name/' + letter + '?num_items=100'
+    html_doc = keep_trying_to_get_html(url)
     soup = BeautifulSoup(html_doc)
 
     # if (soup.find('div', {'class': 'page_nav'})):
@@ -40,3 +40,39 @@ def get_all_movie_critics():
     for letter in lowercase_letters:
         result += get_movie_critics_for_letter(letter)
     return result
+
+def find_by_class(soup, class_, element_type='div'):
+    return soup.find(element_type, attrs={'class': class_})
+
+def get_movie_critic(slug):
+    url = 'http://www.metacritic.com/critic/' + slug
+    html_doc = keep_trying_to_get_html(url)
+    soup = BeautifulSoup(html_doc)
+
+    result = dict()
+
+    result['critic_name'] = find_by_class(soup, 'critic_title').getText().strip()
+    result['publication_title'] = find_by_class(soup, 'publication_title').find('a').getText()
+
+    critscore_stats = find_by_class(soup, 'critscore_stats')
+
+    result['review_count'] = int(find_by_class(critscore_stats, 'label').find('span').getText().replace(' reviews', ''))
+
+    result['percent_higher_than_average'] = int(find_by_class(critscore_stats, 'data stats_score above_average', element_type='span').getText().replace('%', ''))
+    result['percent_same_than_average'] = int(find_by_class(critscore_stats, 'data stats_score average', element_type='span').getText().replace('%', ''))
+    result['percent_lower_than_average'] = int(find_by_class(critscore_stats, 'data stats_score below_average', element_type='span').getText().replace('%', ''))
+
+
+    points_against_average = find_by_class(find_by_class(soup, 'summary'), re.compile(r".*\baverage_value\b.*"), element_type='span').getText()
+
+    # Get just the point value
+    points_against_average_num = float(points_against_average.split(' ')[0])
+
+    # If the critic scores lower than the average, then make the value negative
+    if 'lower' in points_against_average:
+        points_against_average_num *= -1
+    result['points_against_average'] = points_against_average_num
+
+    
+
+    print result
